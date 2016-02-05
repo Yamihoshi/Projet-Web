c6a78bddc672277dc8866fd9c8d13e28a36d50e1
Yamihoshi <a4arkane@gmail.com>
2016-02-05 19:03:58 +0100

    1er Git depuis 1 an
    Jcrois c'est ça qu'il va falloir utiliser

---
+++ BDD Creation/nettoyage.sql
@@ -0,0 +1,11 @@
+DROP TRIGGER IF EXISTS photoValue;
+DROP TRIGGER IF EXISTS demandeValue;
+DROP TRIGGER IF EXISTS touitesRepTrigger;
+DROP TRIGGER IF EXISTS touitesRetTrigger;
+DROP TRIGGER IF EXISTS touitesPrivesTrigger;
+DROP TRIGGER IF EXISTS suivi;
+
+DROP VIEW IF EXISTS VueTouitesPublics,VueTouitesPrives,VueContenusMessages;
+
+DROP TABLE IF EXISTS retouites,touitesReponses,contenuH,contenuA,suivre,touitesPublics,touitesPrives,touitesNormaux,touitos,touites,hashtags,arobases;
+
