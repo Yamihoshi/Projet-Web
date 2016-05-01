194ecb81c5b5bdf787a5f98a8ffdd09c1594d063
Yamihoshi <a4arkane@gmail.com>
2016-05-01 05:24:37 +0200

    LEL

--- index.php
+++ index.php
@@ -12,6 +12,14 @@ include('nav.php');
 
 ?>
 	<div id="pageDisplay">
+		<div id="k_left">
+			<video controls loop autoplay src="k.mp4">K</video>
+		</div>
+
+		<div id="k_right">
+			<video controls loop autoplay src="k.mp4">K</video>
+		</div>
+
 		<div id="nyan_nyan">
 			<img src="nyan.jpg">
 		</div>
