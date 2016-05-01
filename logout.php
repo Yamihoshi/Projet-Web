b4ed487e3d440c56e3ce477b16df175809094fe5
Maxime Lavaste <maxime.lavaste@gmail.com>
2016-05-01 14:56:07 +0200

    deco

--- logout.php
+++ logout.php
@@ -1,14 +1,7 @@
-﻿<html>
-<head>
-	<title>Touiteur - Se Connecter</title>
-	<link rel="stylesheet" href="includes/css/style.css">
-	<script src="includes/js/jquery-1.11.3.min.js"></script>
-</head>
-<body>
+﻿
 <?php
 
 session_start();
-
 	if(!isset($_SESSION['user']))
 	{
 		header('Location: login.php');
@@ -22,5 +15,3 @@ session_start();
 			header('Location: index.php');
 	}
 ?>
-</body>
-</html>
